43ec04dd3248fb6573cef6521b9282578fc75b14
PiyushMishra15 <piyushmishra2915@gmail.com>
2025-01-16 22:57:12 +0530

    Events

--- src/app/Events/page.jsx
+++ src/app/Events/page.jsx
@@ -1,75 +1,135 @@
-import React from 'react'
+"use client"
+import React,{useEffect} from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; 
 
-function Events() {
-
-
-    const prevEvents = [
-
-        {
-            description:
-              "Paradox is a globally acclaimed event that is organized before as well as during NIMBUS by Team.EXE the departmental team of Computer Science and Engineering of NIT Hamirpur. It is an online level based game where the participant looks for a word that is signified by an image/set of images. Hints are provided periodically to help with the process of thinking in the right direction. It has received more than 20,000 downloads from all over the world with 5000 downloads from US and UK.",
-            end_date: "2024-04-14T23:59:59",
-            image_url:
-              "https://res.cloudinary.com/dwwajleyo/image/upload/v1713005699/posters_and_assets/paaradoxx_oqphyo.jpg",
-            
-            key: "1",
-            start_date: "2024-04-13T17:00:00",
-            title: "Paradox 2k24",
-            type: "event",
-            year: 2024,
-          },
-     
-      
-        {
-          id: 7,
-          title: "GAME OF CODES",
-          image_url:
-            "https://res.cloudinary.com/dwwajleyo/image/upload/v1711589992/posters_and_assets/game_of_codes_u14t1y.jpg",
-          start_date: "2023-04-15T14:00:00Z",
-          end_date: "2023-04-15T16:00:00Z",
-          type: "event",
-          description:
-            "Game Of Codes is an online competitive programming contest. This event consists of interesting coding problems which coders love to solve. It's a short coding contest of two and a half hours. Students from NITH, as well as from other institutions, are welcome to participate and win amazing prizes!!",
-          year: 2023,
-        },
-        {
-          id: 8,
-          title: "TRADE AND CODE",
-          image_url:
-            "https://res.cloudinary.com/dwwajleyo/image/upload/v1711589992/posters_and_assets/trade_and_code_ykvzaj.jpg",
-          start_date: "2023-04-15T21:30:00Z",
-          end_date: "2023-04-15T23:00:00Z",
-          type: "event",
-          description:
-            "Trade and Code brings a fun amalgamation of economics and coding. It is an event based on real-time trading and problem solving while managing your balance. Each team will be provided with a certain amount of virtual currency. They have to buy various components and libraries required to write a code (eg: loops, if-else statements, string library, classes, etc). The best coders and traders win exciting prizes!",
-        
-          year: 2023,
-        },
-
-        {
-            description:
-              "Coding Competition is an online competitive programming contest. This event presents a series of intriguing coding challenges that are sure to captivate any programmer's interest. This event will be conducted on codeforces platform and will be open for everyone. However, the event will be on site and conducted in online mode. Participants stand a chance to win exciting rewards! So, come and immerse yourself in this thrilling coding adventure. See the description to know about the rules of the Event.",
-            end_date: "2024-04-11T19:20:00",
-            image_url:
-              "https://res.cloudinary.com/dwwajleyo/image/upload/v1712601007/posters_and_assets/coding-comp_ryy2zo.jpg",
-            key: "3",
-            start_date: "2024-04-11T17:05:00",
-            title: "Coding Competition",
-            type: "event",
-            year: 2024,
-          },
-      ];
-      
-      
-    
-
+const Events = () => {
+  const events = [
+    {
+      id: 1,
+      title: "Paradox 2k24",
+      type: "event",
+      image_url:
+        "https://res.cloudinary.com/dwwajleyo/image/upload/v1713005699/posters_and_assets/paaradoxx_oqphyo.jpg",
+      start_date: "2024-04-14T17:00:00Z",
+      end_date: "2024-04-14T23:59:59Z",
+      description:
+        "Paradox is a globally acclaimed event organized before and during NIMBUS by Team.EXE, the departmental team of Computer Science and Engineering of NIT Hamirpur. It's an online level-based game where participants look for a word signified by an image/set of images, with hints provided periodically.",
+    },
+    {
+      id: 2,
+      title: "Coding Competition",
+      type: "event",
+      image_url:
+        "https://res.cloudinary.com/dwwajleyo/image/upload/v1712601007/posters_and_assets/coding-comp_ryy2zo.jpg",
+      start_date: "2024-04-11T17:05:00Z",
+      end_date: "2024-04-11T20:00:00Z",
+      description:
+        "An online competitive programming contest presenting coding challenges sure to captivate any programmer's interest. Hosted on Codeforces, the event is open for everyone and features exciting rewards for participants!",
+    },
+    {
+      id: 3,
+      title: "Game of Codes",
+      type: "event",
+      image_url:
+        "https://res.cloudinary.com/dwwajleyo/image/upload/v1711589992/posters_and_assets/game_of_codes_u14t1y.jpg",
+      start_date: "2023-04-15T13:30:00Z",
+      end_date: "2023-04-15T16:00:00Z",
+      description:
+        "Game Of Codes is a short, exciting coding contest spanning two and a half hours, open to participants from NITH and other institutions. Solve interesting coding problems and win amazing prizes!",
+    },
+    {
+      id: 4,
+      title: "Trade and Code",
+      type: "event",
+      image_url:
+        "https://res.cloudinary.com/dwwajleyo/image/upload/v1711589992/posters_and_assets/trade_and_code_ykvzaj.jpg",
+      start_date: "2023-04-15T20:00:00Z",
+      end_date: "2023-04-15T23:00:00Z",
+      description:
+        "Trade and Code combines coding with real-time trading. Teams manage a virtual currency to purchase components needed for coding challenges, with the best coders and traders winning exciting prizes!",
+    },
+  ];
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration in milliseconds
+      offset: 100, // Offset before the animation starts
+      easing: "ease-in-out", // Animation easing
+      once: false, // Whether animation should happen only once
+    });
+  }, []);
+  // Check if the event has ended
+  const hasEventEnded = (endDate) => {
+    const currentDate = new Date();
+    return new Date(endDate) < currentDate;
+  };
 
   return (
-    <div>
-      
+    <div
+      className="bg-[url('/6385146.jpg')] bg-cover bg-center text-white min-h-screen"
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8)), url('/6385146.jpg')`,
+      }}
+    >
+      <div className="container mx-auto py-8 px-4">
+        {/* Page Title */}
+        <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-center mb-24 font-lora text-blue-300 drop-shadow-lg"           data-aos="fade-down"
+        >
+          Previous Events
+        </h1>
+
+        {events.map((event) => (
+          <div
+            key={event.id}
+            data-aos="fade-up"
+
+            className="flex flex-col md:flex-row-reverse items-center md:items-start gap-3 mb-40"
+          >
+            {/* Right: Image */}
+            <div data-aos="zoom-out-left"  className="w-full  sm:w-3/4 md:w-1/3 md:mr-12 ">
+              <img
+                src={event.image_url}
+                alt={event.title}
+                className="w-full h-auto object-cover rounded-lg shadow-md spotlight-card"
+              />
+            </div>
+
+            {/* Left: Text Content */}
+            <div data-aos="zoom-out-right" className="w-full ml-8 sm:w-3/4 md:w-2/3">
+              <h2 className="text-3xl sm:text-4xl md:text-5xl font-semibold font-poppins text-blue-300   mb-4">
+                {event.title}
+              </h2>
+              <p className="text-lg sm:text-2xl md:text-3xl text-gray-200 mb-4 font-montserrat md:w-2/3 ">
+                {event.description}
+              </p>
+              <p className="text-xl sm:text-xl md:text-3xl font-playair mb-4">
+                <span className="font-medium text-gray-300">Date:</span>{" "}
+                <span className="text-blue-300">
+                  {new Date(event.start_date)
+                    .toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })
+                    .replace(/,/g, " ")}
+                </span>
+              </p>
+
+              {/* Event Over Button */}
+              {hasEventEnded(event.end_date) && (
+                <button data-aos="flip-right" className="bg-red-600 text-white px-6 py-3 rounded-lg text-lg sm:text-xl font-bold mt-4">
+                  Event Over
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+
+        {/* Footer Line */}
+        <hr className="border-t-2 border-gray-300 my-8" data-aos="flip-in" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Events;
-
